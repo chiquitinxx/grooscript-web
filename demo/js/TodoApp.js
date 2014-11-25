8a103040eb7d5ef27be63f479c6d7412d7d35ec9
Jorge Franco <jorge.franco.leza@gmail.com>
2014-11-25 23:35:27 +0100

    Fix failing demos

--- demo/js/TodoApp.js
+++ demo/js/TodoApp.js
@@ -21,7 +21,7 @@ function TodoApp() {
     };
   }
   gSobject['render'] = function(it) {
-    return gs.mc(gSobject.gQuery,"html",[gSobject.selector, gs.execStatic(HtmlBuilder,'build', this,[function(it) {
+    return gs.mc(gs.execCall(gs.gp(gs.thisOrObject(this,gSobject),"gQuery"), this, [gSobject.selector]),"html",[gs.execStatic(HtmlBuilder,'build', this,[function(it) {
       return gs.mc(this,"form",[gs.map().add("id","addTodos"), function(it) {
         gs.mc(this,"h3",["TODO"], gSobject);
         return gs.mc(this,"ul",[function(it) {
@@ -29,7 +29,7 @@ function TodoApp() {
             return gs.mc(this,"li",[it], gSobject);
           }]);
           return gs.mc(this,"li",[function(it) {
-            gs.mc(this,"input",[gs.map().add("placeholder","add a to-do...").add("type","text").add("id","actualTodo").add("value",gSobject.actualTodo)], gSobject);
+            gs.mc(this,"input",[gs.map().add("type","text").add("id","actualTodo").add("value",gSobject.actualTodo)], gSobject);
             return gs.mc(this,"button",[function(it) {
               return gs.mc(this,"yield",["Add #" + (gs.plus(gs.mc(gSobject.todos,"size",[]), 1)) + ""], gSobject);
             }], gSobject);
