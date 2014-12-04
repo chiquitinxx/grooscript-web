25075c1a034a017c730b3c6a6c02216b25cf5b38
jfranco <jorge.franco.leza@gmail.com>
2014-12-04 13:27:42 +0100

    Conversions

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,17 @@ gulp.task('default', function() {
       .pipe(concat('index.js'))
       .pipe(gulp.dest('js'));
 });
+
+gulp.task('conversion', function() {
+    // place code for your default task here
+    return gulp.src(['demo/js/jquery.min.js',
+        'js/grooscript.min.js',
+        'js/grooscript-tools.js',
+        'js/ace.js',
+        'js/mode-groovy.js',
+        'js/ConvertedCode.js',
+        'js/ConversionPresenter.js'])
+        .pipe(uglify())
+        .pipe(concat('conversion.js'))
+        .pipe(gulp.dest('js'));
+});
